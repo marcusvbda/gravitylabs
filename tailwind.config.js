c449d8550d2ca08ec8f0da81774f65d14cce564b
Vinicius Bassalobre <bassalobre.vinicius@gmail.com>
2024-10-28 16:23:47 -0300

    tema flowbit

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ export default {
         './resources/**/*.blade.php',
         './resources/**/*.js',
         './resources/**/*.vue',
+        "./node_modules/flowbite/**/*.js"
     ],
     theme: {
         extend: {
@@ -53,7 +54,9 @@ export default {
             }
         },
     },
-    plugins: [],
+    plugins: [
+        require('flowbite/plugin')
+    ],
     safelist: [
         'text-green-800 border-green-300 bg-green-50 dark:text-green-400 dark:border-green-800',
         'text-blue-800 bg-blue-50 dark:text-blue-400',
